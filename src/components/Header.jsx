352dfdf5c395bb516d602e4fde01b409d87c599e
Nv22005 <155470674+Nv22005@users.noreply.github.com>
2024-02-25 09:04:03 +0000

    Creating Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { images } from '../constants';
+
+const Header = () => {
+  return (
+  <section>
+    <header>
+      <div><img src={images.Logo} alt="logo" /></div>
+      <div></div>
+    </header>
+  </section>
+  );
+};
+
+export default Header;
