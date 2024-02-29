bd8c92a576a110a60cd1a5fe159d116128bb07c4
Nv22005 <155470674+Nv22005@users.noreply.github.com>
2024-02-29 05:13:41 +0000

    4 - Responsive Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { images } from '../constants';
 
-const NavItemInfo  = [
-  {name: "Home"},
-  {name: "Articles"},
-  {name: "Page"},
-  {name: "Pricing"},
-  {name: "Faq"},
-]
+const Header = () => {
+  const NavItemInfo = [
+    { name: "Home" },
+    { name: "Articles" },
+    { name: "Page" },
+    { name: "Pricing" },
+    { name: "Faq" },
+  ];
 
-const navitem = (name) => {
+const NavItem = ({ name }) => {
   return (
     <li className="relative group">
-      <a href="/" className="px-4 py-2 ">Home</a>
-      <span className='text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 grop-hover:opacity-100'>/</span>
+      <a href="/" className="px-4 py-2">{name}</a>
+      <span className="text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100">/</span>
     </li>
   );
 };
 
-const Header = () => {
   return (
     <section>
       <header className="container mx-auto px-5 flex justify-between py-4 items-center">
@@ -26,12 +26,14 @@ const Header = () => {
           <img src={images.Logo} alt="logo" />
         </div>
         <div className="flex gap-x-9 items-center">
-          <ul className="flex gap-x-5 font-semibold">
-            {NavItemInfo.map((item) => 
-            <navitem key={item.name} name={item.name} />
-            )}
+          <ul className="flex gap-x-2 font-semibold">
+            {NavItemInfo.map((item) => (
+              <NavItem key={item.name} name={item.name} />
+            ))}
           </ul>
-          <button className="border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-seemibold hover:bg-blue-500 hover:text-white transition-all duration-300">Sign in</button>
+          <button className="border-2 border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300">
+            Sign in
+          </button>
         </div>
       </header>
     </section>
