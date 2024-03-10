a86db2acfad9ef829234d803a1883620a04a8ace
Nv22005 <155470674+Nv22005@users.noreply.github.com>
2024-03-10 08:59:17 +0000

    Article card component

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,13 @@
-import Logo from "../assets/Logo.svg";
-import HeroImage from "../assets/HeroImage.svg";
+import Logo from "../assets/posts/Logo.svg";
+import HeroImage from "../assets/posts/HeroImage.svg";
+import Post1Image from "../assets/posts/post-profile.svg";
+import PostProfileImage from "../assets/posts/post-profile.svg";
 
 const images = {
   Logo,
   HeroImage,
+  Post1Image,
+  PostProfileImage,
 };
 
 export default images;
