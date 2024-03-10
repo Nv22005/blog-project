68822a51773825f63e37879e591e9bc42700269d
Nv22005 <155470674+Nv22005@users.noreply.github.com>
2024-03-10 08:06:44 +0000

    Hero Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
         primary: "#1565D8",
         dark: {
+          light: "#5A7184",
           hard: "#0D2436",
           soft: "#183B56",
         },
       },
       fontFamily: {
-        opensans: ["'open sans'", "sans-serif"],
-        roboto: ["'roboto'", "sans-serif"],
+        opensans: ["'Open Sans'", "sans-serif"],
+        roboto: ["'Roboto'", "sans-serif"],
       },
     },
   },
