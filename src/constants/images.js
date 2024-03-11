3e1a9a3019ccb9e4e4ec4ea36a507d80ff794ca2
Nv22005 <155470674+Nv22005@users.noreply.github.com>
2024-03-11 07:43:13 +0000

    Article card component

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,6 +1,6 @@
 import Logo from "../assets/posts/Logo.svg";
 import HeroImage from "../assets/posts/HeroImage.svg";
-import Post1Image from "../assets/posts/post-profile.svg";
+import Post1Image from "../assets/posts/post1.jpg";
 import PostProfileImage from "../assets/posts/post-profile.svg";
 
 const images = {
