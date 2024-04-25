c5b572e4e2073012ed1ae89a34ce447a6bf6a6ee
Nv22005 <155470674+Nv22005@users.noreply.github.com>
2024-04-25 08:38:17 +0000

    21 - post user data to the backend

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import { Routes, Route } from "react-router-dom";
+import {Toaster} from 'react-hot-toast'
 
 import "./App.css";
 import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
 import HomePage from "./pages/home/HomePage";
 import RegisterPage from "./pages/register/RegisterPage";
-
 function App() {
   return (
     <div className="App font-opensans">
       <Routes>
         <Route index path="/" element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
         <Route path="/blog/:id" element={<ArticleDetailPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
-      
+      <Toaster />
     </div>
   );
 }
