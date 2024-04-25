e5be71f7a64eba47d0a747c8768e93d0b9a47301
Nv22005 <155470674+Nv22005@users.noreply.github.com>
2024-04-25 04:45:03 +0000

    20 - register form

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import ArticleDetailPage from "./pages/articleDetail/ArticleDetailPage";
 import HomePage from "./pages/home/HomePage";
+import RegisterPage from "./pages/register/RegisterPage";
 
 function App() {
   return (
     <div className="App font-opensans">
       <Routes>
         <Route index path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/blog/:id" element={<ArticleDetailPage />} />
       </Routes>
+      
     </div>
   );
 }
